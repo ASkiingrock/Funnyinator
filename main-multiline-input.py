01b5f3cd2b769e42d6f87ce069bc0f5f72f02f0c
ASkiingrock <93888719+ASkiingrock@users.noreply.github.com>
2022-04-28 18:27:39 +1000

    Update main-multiline-input.py

--- main-multiline-input.py
+++ main-multiline-input.py
@@ -12,28 +12,3 @@ i = i[0].lower()
 i = " ".join( i.split() )
 
 print(main_func.Funnyinator(i))
-
-# with open("words.json", "r") as inputfile:  # The words to be swapped out are contained in the .JSON file, so they need to be taken out of it, which is what this does
-#     words = json.load(inputfile)
-
-# wordsList = list(words.items())
-
-# for j in range(0, len(wordsList)-1):
-#     i = i.replace(wordsList[j][0], wordsList[j][1])  # This replaces all the words
-
-# i = i.split(" ") # Split the result
-
-
-# for word in i: # This function just adds the funny words periodically
-#     end = " "
-#     if word[-1] not in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ": # takes into account if the word has punctuation or something else at the end of it
-#         end = word[-1] + " "
-#         word = word[:-1]
-        
-#     ran = random.randint(1,25)   # This will add one of the "funny words" in the JSON file randomly every 25 words
-#     output += f"{word}{end}"
-#     if ran == 1:
-#         funny = random.choice(words["funny words"])
-#         output += f"{funny}{end}"
-
-# print(output) # Output the result to the console
